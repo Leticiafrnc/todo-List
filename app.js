484254793cb565507d63a54083089ed1a27f148b
https://github.com/Leticiafrnc <leticiafrnc1@gmail.com>
2021-11-03 12:55:43 -0300

    app.js

--- app.js
+++ app.js
@@ -9,8 +9,52 @@ const criarItem = (tarefa, status) => { /* texto de cada tarefa - status se feit
     <div>${tarefa}</div> 
     <input type="button" value="X">
     `
-    document.getElementById('todoList').appendChild(item)
+    document.getElementById('todoList').appendChild(item) /*add o item criado*/
+}
+
+/*Criando JSON para simular um banco de dados*/
+
+let banco = [
+    {'tarefa': 'Estudar', 'status':''},
+    {'tarefa':'Comer', 'status': 'checked'},
+    {'tarefa':'teste', 'status': 'checked'}
+
+]
+
+/* Le o banco de dados e cria os elementos deles quando mudar os elemtos do banco ele atualiza a tela - Atulizar */
+
+const limparTarefas = () =>{
+    const todoList = document.getElementById('todoList')
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild)
+    }
+}
+
+const atulizarTela = () =>{
+    limparTarefas()
+    banco.forEach(item => criarItem(item.tarefa, item.status)); /*forEach - método do array para percorrer todo o array do banco de dados*/
 }
 
 
+/* função inserir nova tarefa */
+const addItem = (event) => {
+    const tecla = event.key;
+    const texto = event.target.value /*pega a tarefa digitada*/
+    if (tecla==='Enter') {
+        banco.push({'tarefa': texto , 'status': ''})
+        atulizarTela()
+        event.target.value = '' /*limpa a caixa onde são escritas as tarefas*/
+    }
+}
+document.getElementById('newItem').addEventListener('keypress', addItem)
 
+
+/*quando clicar no checked marcar no banco*/
+
+const clikItem = (evento) =>{
+    const elemnto = evento.target;
+}
+document.getElementById('todoList').addEventListener('click', clikItem)
+
+
+atulizarTela()
